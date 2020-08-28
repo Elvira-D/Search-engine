8073be1e57306b5216ed3ce6cbc204e9cb6408b8
Elvira-D <66033824+Elvira-D@users.noreply.github.com>
2020-08-28 18:43:35 +0100

    Imported code from Codesandbox

---
+++ src/Search.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch() {
+    const [city, setCity] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [forecast, setForecast] = useState({});
+
+    function displayWeather(response) {
+        setLoaded(true);
+        setForecast({
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].description,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4820a880cf32920cd905490b92fa0630&units=metric`;
+        axios.get(apiUrl).then(displayWeather);
+    }
+
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+
+    let form = (
+        <form className="search" onSubmit={handleSubmit}>
+            <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+            <button type="Submit">Search</button>
+        </form>
+    );
+
+    if (loaded) {
+        return (
+            <div>
+                {form}
+                <ul>
+                    <li>Temperature: {Math.round(forecast.temperature)}°C</li>
+                    <li>Description: {forecast.description}</li>
+                    <li>Humidity: {forecast.humidity}%</li>
+                    <li>Wind: {forecast.wind}km/h</li>
+                </ul>
+            </div>
+        );
+    } else {
+        return form;
+    }
+}
